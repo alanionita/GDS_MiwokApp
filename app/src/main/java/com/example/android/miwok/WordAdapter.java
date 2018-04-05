08055ccc6e2923adaa69116f34c6b6f3a286367b
alanionita <a.g.ionita@gmail.com>
2018-04-05 10:58:54 +0100

    backup of miwok work

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,27 @@
+package com.example.android.miwok;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by alanionita on 04/04/2018.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        return super.getView(position, convertView, parent);
+    }
+}
