e827f2165c8d31c7c4220e9e4c87d79ea15a927c
Alan Ionita <a.g.ionita@gmail.com>
2018-05-20 00:58:36 +0100

    adds extra styling to the sections and new images to most words

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -6,27 +6,49 @@ package com.example.android.miwok;
 
 public class Word {
     // default translation for the word
-    private String mDefaultWord;
+    private String defaultWord;
 
     // miwok translation for the word
-    private String mMiwokWord;
+    private String miwokWord;
+
+    // resource ID
+    private int imageResourceID = NO_IMAGE_PROVIDED;
+
+    // noimage provided
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     /**
-     * Create a new word object
+     * Create a new word object - primary constructor
      * @param defaultTranslation is the word in a language familiar to the user eg: English
      * @param miwokTranslation is the miwok translation of the default word
      */
 
     public Word(String defaultTranslation, String miwokTranslation) {
-        mDefaultWord = defaultTranslation;
-        mMiwokWord = miwokTranslation;
+        defaultWord = defaultTranslation;
+        miwokWord = miwokTranslation;
+    }
+
+    /**
+     * Secondary contructor for word abject
+     * @param defaultTranslation is the word in a language familiar to the user eg: English
+     * @param miwokTranslation is the miwok translation of the default word
+     * @param imageLocation is the image for each word item
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageLocation) {
+        defaultWord = defaultTranslation;
+        miwokWord = miwokTranslation;
+        imageResourceID = imageLocation;
     }
 
     // define the getters for the variables housed in the class
     public String getDefaultWord () {
-        return mDefaultWord;
+        return defaultWord;
     };
-    public String getMiwokWord () {
-        return mMiwokWord;
-    };
+    public String getMiwokWord () {return miwokWord;};
+    public int getImageResourceID () { return imageResourceID;};
+
+    // returns whether or not the word has an image
+    public boolean hasImage () {
+        return imageResourceID != NO_IMAGE_PROVIDED;
+    }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,21 +1,29 @@
 package com.example.android.miwok;
 
 import android.content.Context;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    WordAdapter(Context context, ArrayList<Word> words) {
+    // global value for the activity color values
+    private int localActivityColor;
+
+    WordAdapter(Context context, ArrayList<Word> words, int activityColor) {
         super(context, 0, words);
+        localActivityColor = activityColor;
     }
 
     @NonNull
@@ -32,9 +40,14 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // Get {@link Word item} at this position in the list
         Word arrayListItem = getItem(position);
 
-        // Find the TextViews from the list_item.xml layout
+        // Find the views from the list_item.xml layout
         TextView miwokWord = (TextView) customView.findViewById(R.id.miwok_word);
         TextView defaultWord = (TextView) customView.findViewById(R.id.default_word);
+        ImageView imageV = (ImageView) customView.findViewById(R.id.image);
+        LinearLayout listWordSection = (LinearLayout) customView.findViewById(R.id.list_word_section);
+
+        // Find the color that the resource id maps to
+        int activityColor = ContextCompat.getColor(getContext(),localActivityColor);
 
         /*
             Set the values in the list_item.xml to the values from the
@@ -44,6 +57,14 @@ public class WordAdapter extends ArrayAdapter<Word> {
         if (arrayListItem != null) {
             miwokWord.setText(arrayListItem.getMiwokWord());
             defaultWord.setText(arrayListItem.getDefaultWord());
+            listWordSection.setBackgroundColor(activityColor);
+            if (arrayListItem.hasImage()) {
+                imageV.setImageResource(arrayListItem.getImageResourceID());
+                imageV.setVisibility(View.VISIBLE);
+            } else {
+                imageV.setVisibility(View.GONE);
+            }
+
         }
 
         // Return the updated list_item view to it can be included in the list view
