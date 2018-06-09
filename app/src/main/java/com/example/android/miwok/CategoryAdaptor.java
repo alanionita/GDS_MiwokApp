f99e2bd3f713b19cf62a2d0ef3d1bd0c7c3b0f00
Alan Ionita <a.g.ionita@gmail.com>
2018-06-09 14:02:02 +0100

    adds tab layout to the viewPager

--- app/src/main/java/com/example/android/miwok/CategoryAdaptor.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdaptor.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -9,25 +10,37 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 
 class CategoryAdaptor extends FragmentPagerAdapter {
+
+    final int PAGE_COUNT = 4;
+    private String tabTitles[] = new String[]{"Numbers", "Family", "Colors", "Phrases"};
+    private Context context;
+
     public CategoryAdaptor(FragmentManager fm) {
         super(fm);
     }
 
     @Override
     public Fragment getItem(int position) {
-        if (position == 0) {
-            return new NumbersFragment();
-        } else if (position == 1) {
-            return new FamilyFragment();
-        } else if (position == 2) {
-            return new ColorsFragment();
-        } else {
-            return new PhrasesFragment();
+        switch (position) {
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            default:
+                return new PhrasesFragment();
         }
     }
 
     @Override
     public int getCount() {
-        return 4;
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
     }
 }
